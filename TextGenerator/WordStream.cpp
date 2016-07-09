d9f8857d0c65ba56854ae04416b0cd716d50945f
Mikhail Matrosov <mikhail.matrosov@gmail.com>
2016-07-09 18:00:52 +0300

    (*) Words are now continuous sequences of letters

--- TextGenerator/WordStream.cpp
+++ TextGenerator/WordStream.cpp
@@ -7,19 +7,23 @@ WordStream::WordStream(std::istream& stream) : m_stream(stream)
 
 WordStream& WordStream::operator>>(std::string& word)
 {
-  while (m_stream >> word)
+  while (m_words.empty())
   {
-    word.erase(
-      std::remove_if(word.begin(), word.end(), !boost::is_alpha()),
-      word.end());
-
-    if (!word.empty())
+    std::string line;
+    if (!std::getline(m_stream, line))
     {
-      break;
+      return *this;
     }
+
+    boost::split(m_words, line, !boost::is_alpha());
+    m_words.erase(
+      std::remove_if(m_words.begin(), m_words.end(), [](const std::string& str) { return str.empty(); }),
+      m_words.end());
   }
 
+  word = std::move(m_words.front());
   boost::to_lower(word);
+  m_words.pop_front();
 
   return *this;
 }
